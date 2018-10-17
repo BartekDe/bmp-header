97f16c9c3b48b79067cea494a861dc68885b0cb5
Bartek Duda <bartekduda01@gmail.com>
2018-10-17 13:03:48 +0200

    initial commit

---
+++ bmp-header.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+	unsigned char bufferOriginal[54];//wil store the header of the original file
+	
+	printf("num of cmd args: %d", argc);
+	for(int i = 0; i < argc; i++) {
+		printf("%d arg: %s \n", i, argv[i]);
+	}
+
+
+
+	FILE *ptrOriginal = fopen(argv[1], "rb");//pointer to the original file
+	FILE *ptrNew = fopen(argv[2], "r+b");//pointer to the file that we overwrite
+
+	if(ptrOriginal == NULL) {
+		printf("couldn't open first file, exiting\n");
+		return 0;	
+	}
+
+	if(ptrNew == NULL) {
+		printf("couldn't open second file, exiting\n");
+		return 0;
+	}
+	
+	fread(bufferOriginal, sizeof(bufferOriginal), 1, ptrOriginal);//saving the original header in the bufferOriginal
+	
+	printf("copied the original header\n");
+	
+	fseek(ptrNew, 0, SEEK_SET);//sets the position to the beginning of the file
+	
+	printf("writing the header to the new file\n");
+	
+	fwrite(bufferOriginal, sizeof(bufferOriginal), sizeof(bufferOriginal), ptrNew);//writing first 54 bytes from the orignal file to new file
+	printf("done.\n");
+return 0;	
+}
